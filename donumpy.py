5b9b0592ac4ac6e9a2bce5ad73aec1490d671cd0
kellyjj <kellyjj@yahoo.com>
2022-09-10 14:29:22 -0700

    1st commit to main

---
+++ donumpy.py
@@ -0,0 +1,9 @@
+#this makes use of venm.  to activate source NumPy/bin/activate
+#this will make use of numpy, tensorflow, and jupiter Notebook.
+
+import numpy as np
+
+
+mylist = [1,2,3,4,5]
+
+np.array(mylist)
